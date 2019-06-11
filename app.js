1049b47164037ee9fe1f08ba2a6ce16cfedd4eeb
Martin Balke <Mrtnbalke@gmail.com>
2019-06-11 13:58:38 -0700

    finished display of results

--- app.js
+++ app.js
@@ -6,55 +6,64 @@ let votes = 25;
 const recentlySeen =[];
 
 //Object Constructor
-//New object for each img with image name, and .type
-//Needs a views and votes property for later data analysis
-function ImageGenerator(name, type){
+function ImageGenerator(product, name, type){
+  this.product = product;
   this.src = `./img/${name}.${type}`;
   this.title = name;
   this.alt = name;
   this.votes = 0;
   this.views = 0;
-  this.votePercent = parseInt(this.votes / this.views);
+  // this.percentage = parseInt(Math.floor(this.views /this.votes) * 100);
+
   allImages.push(this);
 }
 
+
 //Object Instances
-const bag = new ImageGenerator('bag', 'jpg');
-const banana = new ImageGenerator('banana', 'jpg');
-const bathroom = new ImageGenerator('bathroom', 'jpg');
-const boots = new ImageGenerator('boots', 'jpg');
-const breakfast = new ImageGenerator('breakfast', 'jpg');
-const bubblegum = new ImageGenerator('bubblegum', 'jpg');
-const chair = new ImageGenerator('chair', 'jpg');
-const cthulhu = new ImageGenerator('cthulhu', 'jpg');
-const dogDuck = new ImageGenerator('dog-duck', 'jpg');
-const dragon = new ImageGenerator('dragon', 'jpg');
-const pen = new ImageGenerator('pen', 'jpg');
-const petSweep = new ImageGenerator('pet-sweep', 'jpg');
-const scissors = new ImageGenerator('scissors', 'jpg');
-const shark = new ImageGenerator('shark', 'jpg');
-const sweep = new ImageGenerator('sweep', 'png');
-const tauntaun = new ImageGenerator('tauntaun', 'jpg');
-const unicorn = new ImageGenerator('unicorn', 'jpg');
-const usb = new ImageGenerator('usb', 'gif');
+const bag = new ImageGenerator('StarWars Bag','bag', 'jpg');
+const banana = new ImageGenerator('Banana Slicer', 'banana', 'jpg');
+const bathroom = new ImageGenerator('Bathroom Ipad Stand','bathroom', 'jpg');
+const boots = new ImageGenerator('Open Toe Rainboots', 'boots', 'jpg');
+const breakfast = new ImageGenerator('Breakfast Cooker','breakfast', 'jpg');
+const bubblegum = new ImageGenerator('Meatball Bubblegum','bubblegum', 'jpg');
+const chair = new ImageGenerator('Art Deco Chair', 'chair', 'jpg');
+const cthulhu = new ImageGenerator('Cthulhu Action Figure','cthulhu', 'jpg');
+const dogDuck = new ImageGenerator('Dog Duck Bill','dog-duck', 'jpg');
+const dragon = new ImageGenerator('Canned Dragon Meat','dragon', 'jpg');
+const pen = new ImageGenerator('Pen Utensils','pen', 'jpg');
+const petSweep = new ImageGenerator('Pet Sweeper','pet-sweep', 'jpg');
+const scissors = new ImageGenerator('Pizza Scissors','scissors', 'jpg');
+const shark = new ImageGenerator('Shark Blanket','shark', 'jpg');
+const sweep = new ImageGenerator('Baby Sweeper','sweep', 'png');
+const tauntaun = new ImageGenerator('Star Wars Blanket','tauntaun', 'jpg');
+const unicorn = new ImageGenerator('Unicorn Meat','unicorn', 'jpg');
+const usb = new ImageGenerator('Monster USB','usb', 'gif');
 
 //Event handlers
-//25 votes
-//Needs to generate a new image every time the user votes
-//Render those images to the dom (using a function?)
 function votingMachine(event){
   renderImages();
   let target = event.target.alt;
-  console.log(target);
   for(let i = 0; i < allImages.length; i++){
     if (target === allImages[i].alt){
       allImages[i].votes++;
-      console.log(allImages[i].votes);
     }
   }
   votes--;
   if(votes === 0){
+    renderList();
     div_images.removeEventListener('click', votingMachine);
+  }
+}
+
+//functions
+function renderList(){
+  const ul_Results = document.createElement('ul');
+  div_images.appendChild(ul_Results);
+
+  for(let i = 0; i < allImages.length; i++){  
+    const li_Image = document.createElement('li');
+    li_Image.textContent = `${allImages[i].product} got ${allImages[i].votes} votes.`;
+    ul_Results.appendChild(li_Image);
   }
 }
 
@@ -77,7 +86,7 @@ function randomImage(){
   let random = randNum(0, allImages.length -1);
   let image = allImages[random];
   while(recentlySeen.includes(image)){
-    let random = randNum(0, allImages.length -1);
+    random = randNum(0, allImages.length -1);
     image = allImages[random];
   }
   recentlySeen.push(image);
